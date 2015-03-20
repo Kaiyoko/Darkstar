e9696f7686c685f4ccc7f6f3dbd10b2db65fbece
nesstea <uber_ness@hotmail.com>
2015-03-19 17:19:01 -0700

    Added Mob lua files Setting SUPERLINK to 0

---
+++ scripts/zones/Boneyard_Gully/mobs/Swift_Hunter.lua
@@ -0,0 +1,42 @@
+-----------------------------------
+-- Area: Boneyard Gully
+-- NPC:  Swift_Hunter
+-- ID: 	16810002
+-----------------------------------
+-----------------------------------
+
+require("scripts/globals/titles");
+require("scripts/globals/status");
+require("scripts/globals/magic");
+
+
+-----------------------------------
+-- onMobInitialize Action
+-----------------------------------
+
+function onMobInitialize(mob)
+end;
+
+-----------------------------------
+-- onMobSpawn Action
+-----------------------------------
+
+function onMobSpawn(mob)
+	mob:setTP(100);
+	mob:setMobMod(MOBMOD_SUPERLINK,0);
+end;
+
+-----------------------------------
+-- onMobEngaged
+-----------------------------------
+function onMobEngaged(mob, killer)
+	mob:useMobAbility(22); -- Pit Ambush
+end;
+
+
+-----------------------------------
+-- onMobDeath
+-----------------------------------
+
+function onMobDeath(mob, killer)
+end;
